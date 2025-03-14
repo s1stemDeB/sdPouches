bb8c3f9aa4f5764782772a626dc392eef5ed1531
s1stemdev <admin@riven-dell.ru>
2025-03-14 16:38:50 +0500

    Initial commit

---
+++ src/main/java/ru/s1stemdeb/sdPouches/SdPouches.java
@@ -0,0 +1,17 @@
+package ru.s1stemdeb.sdPouches;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SdPouches extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
